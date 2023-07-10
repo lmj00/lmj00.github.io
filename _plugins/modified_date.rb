28181c90a26ddd1977bc6ac7e8d66d48075e4562
이민진 <e.minjin00@gmail.com>
2023-07-11 08:47:19 +0900

    modified_date.rb 조건 추가
    특정 파일들의 최근 수정일이 복구되지 않아, 2023년 7월 11일 9시 이후로 수정된 파일만
    modified_date가 추가되게 함"

--- _plugins/modified_date.rb
+++ _plugins/modified_date.rb
@@ -1,7 +1,10 @@
 class ModifiedDateGenerator < Jekyll::Generator
-    def generate(site)
-        site.posts.docs.each do |post|
-            post.data["modified_date"] = File.mtime(post.path)
-        end
+  def generate(site)
+    after_date = Time.new(2023, 7, 11, 9)
+    site.posts.docs.each do |post|
+      if File.mtime(post.path) > after_date
+        post.data["modified_date"] = File.mtime(post.path)
+      end
     end
-end 
+  end
+end
